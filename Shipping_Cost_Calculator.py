96ef304d2407d6b34fbc0e1545d14fb2c63ac3ec
Alankar Yannam <87337992+yalankar@users.noreply.github.com>
2026-02-19 02:16:15 +0530

    Typo was fixed on Shipping_Cost_Calculator.py file

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,5 +10,5 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by yalankar
+ # Here is a new update by alankar yannam
 
